49e15c2f75bc78e4d901c5a23a298af43edc01a8
Adriel Isaiah Amoguis <adriel.isaiah.amoguis@gmail.com>
2023-02-27 14:57:56 +0800

    Finished line-wise parser

--- input_parser.py
+++ input_parser.py
@@ -8,11 +8,33 @@ class InputParser:
         self.contexts = {"GLOBAL": {}, "DATA": {}, "LOGIC": {}}
         self.current_context_space = "GLOBAL"
 
+    def parse(self):
+        # Check syntax
+        self.check_syntax()
+
+        # Parse
+        aux_data = self.parse_data()
+        logic = self.parse_logic()
+
+        return { aux_data, logic }
+
+    def parse_data(self):
+        # Parse the data section
+        pass
+
+    def parse_logic(self):
+        # Parse the logic section
+        pass
+
     def check_syntax(self):
         # Check for syntax errors
         for i in range(len(self.input_lines)):
             # Check for empty lines
             if self.input_lines[i] == "":
+                continue
+
+            # Ignore if comment
+            if re.match("\s*//", self.input_lines[i]):
                 continue
 
             # Check if we're in a non-global context
@@ -30,7 +52,7 @@ class InputParser:
                     continue
 
                 # Succeeding lines must be data structure declarations
-                if not re.match("(STACK|QUEUE|TAPE)\s[a-zA-Z0-9]*", self.input_lines[i]):
+                if not re.match("(STACK|QUEUE|TAPE)\s[a-zA-Z0-9_]*", self.input_lines[i]):
                     raise SyntaxError("Expected data structure declaration at line " + str(i + 1) + " in context " + self.current_context_space + ". Got " + self.input_lines[i])
 
             elif self.current_context_space == "LOGIC":
@@ -40,7 +62,9 @@ class InputParser:
                     continue
 
                 # Succeeding lines must be logic instructions
-                
+                if not re.match("[a-zA-Z0-9_]*] (SCAN|PRINT|SCAN RIGHT|SCAN LEFT|READ|WRITE|RIGHT|LEFT|UP|DOWN)", self.input_lines[i]):
+                    raise SyntaxError("Expected logic instruction at line " + str(i + 1) + " in context " + self.current_context_space + ". Got " + self.input_lines[i])
 
             else:
                 raise NameError("Unknown context space: " + self.current_context_space)
+            
